09d05ff7282d838e2234891f535401c102415f43
Austin Shaffer <ajshaffer@coastal.edu>
2023-06-08 16:47:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      connectParticles: true,
+      maxParticles: 150,
+      minDistance: 100,
+      sizeVariations: 5,
+      color: '#00C9B1'
+    });
+  };
+
